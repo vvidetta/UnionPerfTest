10919a476da0b30a1547830ddd71a2701dcf487e
Vito Videtta <vvidetta@yahoo.co.uk>
2023-05-29 11:38:14 +0100

    Implement performance testing framework signatures

--- union_perf_test/union_perf_test.cpp
+++ union_perf_test/union_perf_test.cpp
@@ -1,6 +1,27 @@
+#include <chrono>
 #include <iostream>
+#include <set>
+
+using perf_trial_result = std::chrono::milliseconds;
+using union_size_func = int64_t(std::vector<std::set<int>> const&);
+
+template <
+  typename Func,
+  typename... Args
+>
+[[nodiscard]] auto run_performance_trial(Func&& func, Args&&... args) noexcept -> perf_trial_result
+{
+  return {};
+}
+
+auto baseline_func(std::vector<std::set<int>> const& sets) -> int64_t
+{
+  return 0Ui64;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+  auto sets = std::vector<std::set<int>>{};
+  auto result = run_performance_trial(baseline_func, sets);
+  std::cout << "baseline performance (ms) = " << result.count();
 }
